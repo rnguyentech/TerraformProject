9cf6ba4975a1664ad6882ca3663e63d5a920b79d
Ryan Nguyen <ryan@rnguyen.tech>
2025-04-15 22:59:49 -0500

    test 2

--- main.tf
+++ main.tf
@@ -121,13 +121,11 @@ module "container_group" {
     }
   ]
 
-  image_registry_credential = [
-    {
-      server   = var.docker_registry_server
-      username = var.docker_registry_username
-      password = var.docker_registry_password
-    }
-  ]
+  image_registry_credential = {
+    server   = var.docker_registry_server
+    username = var.docker_registry_username
+    password = var.docker_registry_password
+  }
 
   diagnostics_log_analytics = {
     workspace_id  = azurerm_log_analytics_workspace.this.workspace_id
