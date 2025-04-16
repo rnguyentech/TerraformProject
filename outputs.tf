783dd4b2c6e623f05474027d41bf840dfe6872c7
Ryan Nguyen <ryan@rnguyen.tech>
2025-04-15 19:15:58 -0500

    changed outputs and terraform.tfvars

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,15 @@
-output "container_group_name" {
-  value = module.container_group.container_group_name
-}
-
-output "fqdn" {
-  value = module.container_group.fqdn
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
 }
 
 output "log_analytics_workspace_id" {
   value = azurerm_log_analytics_workspace.this.id
 }
 
-output "key_vault_uri" {
+output "container_group_name" {
+  value = module.container_group.container_group_name
+}
+
+output "key_vault_url" {
   value = azurerm_key_vault.keyvault.vault_uri
 }
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,9 +1,7 @@
-# Override any defaults here.  Example:
-
-location           = "West US 2"
-rg_name            = "terraformproject-dev"
-project_prefix     = "demo"
-secret_ttl_hours   = 720         # 30 days
-container_image    = "nginx:1.27-alpine"
-container_cpu      = 1
-container_memory   = 1
+project_prefix = "myproject"
+rg_name        = "terraformproject-dev"
+location       = "West US 2"
+container_image = "nginx:latest"
+container_cpu   = "1"
+container_memory = "2"
+secret_ttl_hours = "48"
